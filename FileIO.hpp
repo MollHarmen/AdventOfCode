4103c4174a7a653c4f160706c0c99c9c0fb6eb78
Harmen Moll <harmen.moll@delem.com>
2022-12-01 09:20:28 +0100

    Added CMakeLists with FileIO utilities and main.cpp

---
+++ FileIO.hpp
@@ -0,0 +1,46 @@
+#ifndef FILEIO_HPP
+#define FILEIO_HPP
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace Utils
+{
+    class FileIo
+    {
+    public:
+        FileIo(const std::string pathToFile)
+        :file(pathToFile)
+        {
+        }
+
+        ~FileIo()
+        {
+            file.close();
+        }
+
+        std::vector<std::string> GetFileContent()
+        {
+            if (file.is_open()) 
+            {
+                std::vector<std::string> fileContent;
+                std::string line;
+                while(std::getline(file, line))
+                {
+                    fileContent.push_back(line);
+                }
+                
+                return fileContent;
+            }
+
+            return {};
+        }
+
+    private:
+        std::ifstream file;
+    };
+}
+
+#endif
