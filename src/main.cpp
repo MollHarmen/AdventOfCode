2c75e45a0bd40bc596577f0c91fe1623ae4efea5
HarmenM <harmen.moll@delem.com>
2022-12-07 07:15:14 +0100

    Added implementation Day 7 2022

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,70 @@
 #include <iostream>
-#include <numeric>
 #include <string>
 #include <vector>
-#include <utility>
 
 #include "Utils/FileIO.hpp"
 
-typedef std::pair<int, int> Section;
-typedef std::pair<Section, Section> SectionPair;
-
-Section GetSectionFromString(const std::string& input)
+struct FileSystemItem
 {
-    constexpr size_t dashOffset = 1;
-    const int min = std::stoi(input.substr(0, input.find('-')));
-    const int max = std::stoi(input.substr(input.find('-')+dashOffset, input.size()));
+    FileSystemItem(const std::string name)
+    : mName(name)
+    {}
 
-    return std::make_pair(min, max);
+    const std::string mName;
+    int size = 0;
+};
+
+struct File : public FileSystemItem
+{
+};
+
+struct Directory : public FileSystemItem
+{
+    Directory(const std::string name, FileSystemItem* upper)
+    : FileSystemItem(name)
+    , mUpper(upper)
+    {}
+
+    const FileSystemItem* mUpper;
+    const std::vector<FileSystemItem> mDiretoryContent;
+};
+
+const std::string cdKey = "$ cd ";
+
+bool IsChangeDirectoryCommand(const std::string& line)
+{
+    return line.find("$ cd ") != line.npos;
 }
 
-void AddSectionsOfLine(const std::string& line, std::vector<Section>& sections)
+int GetFileSystemStructureFromCommandLine(const std::vector<std::string>& commandLineOutput)
 {
-    constexpr size_t commaOffset = 1;
-    std::string firstSection = line.substr(0, line.find(','));
-    std::string secondSection = line.substr(line.find(',')+commaOffset, line.size());
-
-    sections.push_back(GetSectionFromString(firstSection));
-    sections.push_back(GetSectionFromString(secondSection));
-}
-
-SectionPair GetSectionPairFromLine(const std::string& line, std::vector<Section>& sections)
-{
-    constexpr size_t commaOffset = 1;
-    std::string firstSection = line.substr(0, line.find(','));
-    std::string secondSection = line.substr(line.find(',')+commaOffset, line.size());
-
-    return std::make_pair(firstSection, secondSection);
-}
-
-std::vector<Section> GetSectionsFromFile()
-{
-    Utils::FileIo fileIo("../src/day4-input.txt");
-    const auto filePerLine = fileIo.GetFileContent();
-
-    std::vector<Section> sections;
-    for(const auto& line : filePerLine)
+    std::vector<FileSystemItem> fileSystem;
+    for(const auto& line : commandLineOutput)
     {
-        AddSectionsOfLine(line, sections);
+        if(line[0] == '$')
+        {
+            if(IsChangeDirectoryCommand(line))
+            {
+                const std::string cdTarget = line.substr(cdKey.length(), line.length());
+                std::cout << "\tChange Dir to " << cdTarget << "\n";
+            }
+        }
+        else
+        {
+            std::cout << "Result: " << line << "\n";
+        }
     }
 
-    return sections;
-}
-
-bool IsInRange(const int number, const int min, const int max)
-{
-    return number >= min && number <= max;
-}
-
-bool DoesSectionFitWithinOtherSection(const Section& section, const Section& other)
-{
-    const bool isLowerSectionInRange = IsInRange(std::get<0>(section), std::get<0>(other), std::get<1>(other));
-    const bool isUpperSectionInRange = IsInRange(std::get<1>(section), std::get<0>(other), std::get<1>(other));
-
-    return isLowerSectionInRange && isUpperSectionInRange;
+    return 0;
 }
 
 void FirstPart()
 {
     std::cout << "= First part =\n";
-    const auto sections = GetSectionsFromFile();
-    for(const auto& section : sections)
-    {
-        for(const auto& compareSection : sections)
-        {
-            if(compareSection != section)
-            {
-                const bool isInSectionRange = DoesSectionFitWithinOtherSection(section, compareSection);
-                
-                if(isInSectionRange)
-                {   
-                    std::cout << std::get<0>(section) << "-" << std::get<1>(section) << " AND ";
-                    std::cout << std::get<0>(compareSection) << "-" << std::get<1>(compareSection);
-                    std::cout << " = Yes\n";
-                }
-           }
-        }
-    }
+    Utils::FileIo fileIo("../src/day7-input.txt");
+    const auto filePerLine = fileIo.GetFileContent();
+
+    const auto fileSystemStructure = GetFileSystemStructureFromCommandLine(filePerLine);
 }
 
 void SecondPart()
