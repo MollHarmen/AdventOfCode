0aa7efa25fd228fd8d9d9e0df4f96c4499c05e35
HarmenM <harmen.moll@delem.com>
2022-12-09 07:37:12 +0100

    intermediate

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,140 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "Utils/FileIO.hpp"
 
-struct FileSystemItem
+struct Head
 {
-    FileSystemItem(const std::string name)
-    : mName(name)
-    {}
-
-    const std::string mName;
-    int size = 0;
-};
-
-struct File : public FileSystemItem
-{
-};
-
-struct Directory : public FileSystemItem
-{
-    Directory(const std::string name, FileSystemItem* upper)
-    : FileSystemItem(name)
-    , mUpper(upper)
-    {}
-
-    const FileSystemItem* mUpper;
-    const std::vector<FileSystemItem> mDiretoryContent;
-};
-
-const std::string cdKey = "$ cd ";
-
-bool IsChangeDirectoryCommand(const std::string& line)
-{
-    return line.find("$ cd ") != line.npos;
-}
-
-int GetFileSystemStructureFromCommandLine(const std::vector<std::string>& commandLineOutput)
-{
-    std::vector<FileSystemItem> fileSystem;
-    for(const auto& line : commandLineOutput)
+    void Move(const std::string& direction)
     {
-        if(line[0] == '$')
+        previousPosition = std::make_pair(x, y);
+
+        if(direction == "U")
         {
-            if(IsChangeDirectoryCommand(line))
-            {
-                const std::string cdTarget = line.substr(cdKey.length(), line.length());
-                std::cout << "\tChange Dir to " << cdTarget << "\n";
-            }
+            ++y;
+        } 
+        else if(direction == "D")
+        {
+            --y;
         }
-        else
+        else if(direction == "L")
         {
-            std::cout << "Result: " << line << "\n";
+            --x;
+        }
+        else if(direction == "R")
+        {
+            ++x;
         }
     }
 
-    return 0;
-}
+    int x = 0;
+    int y = 0;
+
+    std::pair<int,int> previousPosition;
+};
+
+struct Tail
+{
+    Tail()
+    {
+        AddCurrentPossitionToVisited();
+    }
+
+    void AddCurrentPossitionToVisited()
+    {
+        positionVisited[std::to_string(mX) + std::to_string(mY)] = 1;
+    }
+
+    void Move(const int x, const int y)
+    {
+        mX = x;
+        mY = y;
+        AddCurrentPossitionToVisited();
+    }
+
+    bool NeedsToMove(const int x, const int y)
+    {
+        return std::abs(x - mX) > 1 || std::abs(y - mY) > 1;
+    }
+
+    int GetNumberOfPositionsVisited()
+    {
+        return positionVisited.size();
+    }
+
+    int mX = 0;
+    int mY = 0;
+    std::map<std::string, int> positionVisited;
+};
 
 void FirstPart()
 {
     std::cout << "= First part =\n";
-    Utils::FileIo fileIo("../src/day7-input.txt");
+    Utils::FileIo fileIo("../src/day9-input.txt");
     const auto filePerLine = fileIo.GetFileContent();
 
-    const auto fileSystemStructure = GetFileSystemStructureFromCommandLine(filePerLine);
+    Head head;
+    Tail tail;
+
+    for(const auto& line : filePerLine)
+    {
+        const std::string direction = line.substr(0, 1);
+        const int numberOfSteps = std::stoi(line.substr(2, line.length()));
+
+        for(int stepsTaken = 0; stepsTaken != numberOfSteps; ++stepsTaken)
+        {
+            head.Move(direction);
+            if(tail.NeedsToMove(head.x, head.y))
+            {
+                tail.Move(std::get<0>(head.previousPosition), std::get<1>(head.previousPosition));
+            }
+        }
+    }
+
+    std::cout << "\tNumber of unique position visited by tail: " << std::to_string(tail.GetNumberOfPositionsVisited()) << "\n";
 }
 
 void SecondPart()
 {
     std::cout << "= Second part =\n";
+    Utils::FileIo fileIo("../src/day9-input.txt");
+    const auto filePerLine = fileIo.GetFileContent();
+
+    Head head;
+    std::array<Tail, 9> tails = {};
+
+    for(const auto& line : filePerLine)
+    {
+        const std::string direction = line.substr(0, 1);
+        const int numberOfSteps = std::stoi(line.substr(2, line.length()));
+
+        for(int stepsTaken = 0; stepsTaken != numberOfSteps; ++stepsTaken)
+        {
+            head.Move(direction);
+            for(int index = 0; index != tails.size(); ++index)
+            {
+                if(index = 0)
+                {
+                    if(tails[index].NeedsToMove(head.x, head.y))
+                    {
+                        tails[index].Move(head.x, head.y);
+                    }
+                }
+                else
+                {
+                    if(tails[index].NeedsToMove(tails[index-1].mX, tails[index-1].mY))
+                    {
+                        tails[index].Move(tails[index-1].mX, tails[index-1].mY);
+                    }   
+                }
+            }   
+        }
+    }
+    std::cout << "\tNumber of unique position visited by tail: " << std::to_string(tails[8].GetNumberOfPositionsVisited()) << "\n";
 }
 
 int main()
