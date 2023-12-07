425edb37e07bb9d45ea92068c05dac8d4d886a9c
Harmen Moll <harmen.moll@delem.com>
2023-12-07 07:40:43 +0100

    Intermediate day 7 2023

--- src/main.cpp
+++ src/main.cpp
@@ -1,177 +1,145 @@
 #include <algorithm>
-#include <array>
 #include <iostream>
-#include <map>
-#include <string>
+#include <sstream>
 #include <vector>
 
 #include "Utils/FileIO.hpp"
 #include "Utils/StringHandling.hpp"
 
-bool IsCharacterAGear(const char& character)
+const std::string CharacterValueOrder = "23456789TJQKA";
+struct
 {
-    return character == '*';
+    bool operator()(const char& left, const char& right) const 
+    {
+        return CharacterValueOrder.find(left) > CharacterValueOrder.find(right); 
+    }
+} cardCompare;
+
+const std::vector<char> patternChars {'X', 'Y', 'Z', 'A', 'B'};
+
+
+
+struct Hand
+{
+    std::vector<char> mHand;
+    const int mBid;
+    const std::string mPattern;
+
+    Hand(std::vector<char> hand, const int bid)
+    : mHand(hand)
+    , mBid(bid)
+    , mPattern(GetHandPattern())
+    {
+        std::sort(mHand.begin(), mHand.end(), [&hand](const char& left, const char& right)
+        {
+            if(std::count(hand.begin(), hand.end(), left) == std::count(hand.begin(), hand.end(), right))
+            {
+                return CharacterValueOrder.find(left) > CharacterValueOrder.find(right);
+            }
+
+            return std::count(hand.begin(), hand.end(), left) > std::count(hand.begin(), hand.end(), right);
+        });
+    }
+    
+    void Print() const
+    {
+        const std::string hand(mHand.begin(), mHand.end());
+        std::cout << hand << " " << mPattern << " " << mBid << "\n";
+    }
+
+    std::string GetHandPattern() const
+    {
+        char previousChar = *mHand.begin();
+        size_t patternCharacterIndex = 0;
+        std::stringstream pattern;
+
+        for(auto cardIterator = mHand.begin(); cardIterator != mHand.end(); ++cardIterator)
+        {
+            if(*cardIterator == previousChar)
+            {
+                pattern << patternChars[patternCharacterIndex];
+            }
+            else
+            {
+                pattern << patternChars[++patternCharacterIndex];
+            }
+            previousChar = *cardIterator;
+        }
+
+        return pattern.str();
+    }
+
+    char GetHighestCard()
+    {
+        return mHand[0];
+    }
+
+    bool operator<(const Hand& other)
+    {
+        return mPattern < other.mPattern;
+    }
+};
+
+typedef std::vector<Hand> Hands;
+
+std::vector<char> SplitStringIntoChars(const std::string& input)
+{
+    std::vector<char> characters;
+    for(const auto& character : input)
+    {
+        characters.push_back(character);
+    }
+
+    return characters;
 }
+
+Hand GetHandFromLine(const std::string& line)
+{
+    std::vector<std::string> split;
+    StringHandling::SplitStringOnCharacter(line, split, ' ');
+
+    return {SplitStringIntoChars(split[0]), std::stoi(split[1])};
+}
+
+Hands GetHandsFromInput(const std::vector<std::string>& input)
+{
+    Hands hands;
+    for(const auto& line : input)
+    {
+        hands.push_back(GetHandFromLine(line));
+    }
+
+    return hands;
+}
+
 
 void FirstPart()
 {
     std::cout << "= First part =\n";
     Utils::FileIo fileIo("C:/projects/AdventOfCode/src/input.txt");
     const std::vector<std::string> filePerLine = fileIo.GetFileContent();
-}
-
-int GetGearNumber(const std::string& line, const int characterIndex)
-{
-    int start = characterIndex;
-    int stop = characterIndex;
-
-    while(isdigit(line[start]) && start > 0)
-    {
-        --start;   
-    }
-
-    if (!isdigit(line[start]))
-    {
-        ++start;
-    }
-
-    while(isdigit(line[stop]) && stop < line.size()-1)
-    {
-        ++stop;
-    }
-
-    if (!isdigit(line[stop]))
-    {
-        --stop;
-    }
-
-    return std::stoi(line.substr(start, stop+1));
-}
-
-std::vector<int> GetGearNumbering(const std::vector<std::string> grid, const int lineIndex, const int characterIndex)
-{
-    std::vector<int> gearNumbers;
-    bool foundUp = false;
-    bool foundDown = false;
-
-    // look up for digit
-    if(lineIndex > 0)
-    {
-        // Up
-        if(isdigit(grid[lineIndex-1][characterIndex]))
-        {
-            gearNumbers.push_back(GetGearNumber(grid[lineIndex-1], characterIndex));
-            foundUp = true;
-        }
-    }
-
-    if(lineIndex > 0 && !foundUp)
-    {
-        // Left up diagonal
-        if(characterIndex > 0)
-        {
-            if(isdigit(grid[lineIndex-1][characterIndex-1]))
-            {
-                gearNumbers.push_back(GetGearNumber(grid[lineIndex-1], characterIndex-1));
-            }
-        }
-
-        // Right up diagonal
-        if(characterIndex < (grid[lineIndex-1].size()-1))
-        {
-            if(isdigit(grid[lineIndex-1][characterIndex+1]))
-            {
-                gearNumbers.push_back(GetGearNumber(grid[lineIndex-1], characterIndex+1));
-            }
-        }
-    }
-
-    // look down for digit
-    if(lineIndex < (grid.size()-1))
-    {
-        // Down
-        if(isdigit(grid[lineIndex+1][characterIndex]))
-        {
-            gearNumbers.push_back(GetGearNumber(grid[lineIndex+1], characterIndex));
-            foundDown = true;
-        }
-    }
     
-    if(lineIndex < (grid.size()-1) && !foundDown)
+    Hands hands = GetHandsFromInput(filePerLine);
+    for(const auto& hand : hands)
     {
-        // Left down diagonal
-        if(characterIndex > 0)
-        {
-            if(isdigit(grid[lineIndex+1][characterIndex-1]))
-            {
-                gearNumbers.push_back(GetGearNumber(grid[lineIndex+1], characterIndex-1));
-            }
-        }
-
-        // Right down diagonal
-        if(characterIndex < (grid[lineIndex+1].size()-1))
-        {
-            if(isdigit(grid[lineIndex+1][characterIndex+1]))
-            {
-                gearNumbers.push_back(GetGearNumber(grid[lineIndex+1], characterIndex+1));
-            }
-        }
+        hand.Print();
     }
-
-    // Left
-    if(characterIndex > 0)
-    {
-        if(isdigit(grid[lineIndex][characterIndex-1]))
-        {
-            gearNumbers.push_back(GetGearNumber(grid[lineIndex], characterIndex-1));
-        }
-    }
-
-    // Right 
-    if(characterIndex < (grid[lineIndex].size()-1))
-    {
-        if(isdigit(grid[lineIndex][characterIndex+1]))
-        {
-            gearNumbers.push_back(GetGearNumber(grid[lineIndex], characterIndex));
-        }
-    }
-
-    return gearNumbers;
+    // std::sort(hands.begin(), hands.end(), cardCompare);
 }
-
 void SecondPart()
 {
     std::cout << "= Second part =\n";
     Utils::FileIo fileIo("C:/projects/AdventOfCode/src/input.txt");
     const std::vector<std::string> filePerLine = fileIo.GetFileContent();
 
-    int sumOfParts = 0;
-    for(size_t lineIndex = 0u; lineIndex != filePerLine.size(); ++lineIndex)
-    {
-        const auto& line = filePerLine[lineIndex];
-
-        for(size_t characterIndex = 0u; characterIndex != line.length(); ++characterIndex)
-        {
-            if(IsCharacterAGear(line[characterIndex]))
-            {
-                const std::vector<int> gearRatio = GetGearNumbering(filePerLine, lineIndex, characterIndex);
-                if (gearRatio.size() > 1)
-                {
-                    std::cout << gearRatio[0] << " * " << gearRatio[1] << "\n";
-                    sumOfParts += (gearRatio[0] * gearRatio[1]);
-                }
-            }
-        }
-    }
-    std::cout << "Sum of gear ratios: " << sumOfParts << "\n";
 }
 
 int main()
 {
     std::cout << "Advent of code main\n";
 
-    // FirstPart();
-    SecondPart();
+    FirstPart();
+    // SecondPart();
 
     return 0;
 }
