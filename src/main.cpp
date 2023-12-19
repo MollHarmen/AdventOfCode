4c248b6c82ebc9f458fdecc7b98c742084c6d398
Harmen Moll <harmen.moll@delem.com>
2023-12-19 09:22:04 +0100

    Intermediate day 19

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,65 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
-#include <numeric>
+#include <stdexcept>
 #include <vector>
 
 #include "Utils/FileIO.hpp"
 #include "Utils/StringHandling.hpp"
 
-typedef std::tuple<std::string, std::string> Node;
-typedef std::map<std::string, Node> Network;
-
-Node GetNodeFromString(const std::string& input)
+struct Trench
 {
-    std::string copy = input;
-    copy.erase(0, 1);
-    copy.erase(copy.size() - 1);
+    Trench(const int x, const int y, const std::string color)
+    : mX(x)
+    , mY(y)
+    , mColor(color)
+    {}
 
-    std::vector<std::string> split;
-    StringHandling::SplitOnString(copy, split, ", ");
+    bool operator<(const Trench& other)
+    {
+        return mX < other.mX;
+    }
 
-    return std::make_tuple(split[0], split[1]);
+    bool operator==(const Trench& other)
+    {
+        return mX == other.mX && mY == other.mY;
+    }
+
+    int mX;
+    int mY;
+    std::string mColor;
+
+    Trench* mNabour;
+    int mDistance = 0;
+};
+
+typedef std::map<int, std::vector<Trench>> Trenches;
+
+int GetTrenchInteriorSize(Trenches& trenches)
+{
+    int accumulate = 0;
+    for(auto& rowTuple : trenches)
+    {
+        auto& row = std::get<1>(rowTuple);
+        std::sort(row.begin(), row.end());
+
+        const int distanceToLeft = row[row.size()-1].mX - row[0].mX +1;
+        accumulate += distanceToLeft;
+    }
+
+    return accumulate;
+}
+
+void AddTrenchesInYRanche(Trenches& trenches, const int x, const int yStart, const int numberOfTrenches, const std::string color)
+{
+    for(int index = yStart; index != yStart+numberOfTrenches+1; ++index)
+    {
+        const auto found = std::find_if(trenches[index].begin(), trenches[index].end(), [&x](const auto& trench){ return trench.mX == x;});
+        if(found == trenches[index].end())
+        {
+            trenches[index].push_back({x, index, color});
+        }
+    }
 }
 
 void FirstPart()
@@ -28,84 +68,48 @@ void FirstPart()
     Utils::FileIo fileIo("C:/projects/AdventOfCode/src/input.txt");
     const std::vector<std::string> filePerLine = fileIo.GetFileContent();
 
-    std::string instructions;
-    Network network;
+    Trenches trenches;
 
+    int x = 0;
+    int y = 0;
     for(const auto& line : filePerLine)
     {
-        if(instructions.length() == 0)
+        std::vector<std::string> split;
+        StringHandling::SplitStringOnCharacter(line, split, ' ');
+
+        const std::string& direction = split[0];
+        const int distance = std::stoi(split[1]);
+        const std::string color = split[2];
+
+        if(direction == "R")
         {
-            instructions = line;
+            x += distance;
+            trenches[y].push_back({x, y, color});
+        }
+        else if(direction == "L")
+        {
+            x -= distance;
+            trenches[y].push_back({x, y, color});
+        }
+        else if(direction == "U")
+        {
+            AddTrenchesInYRanche(trenches, x, y - distance, distance, color);
+            y -= distance;
+        }
+        else if(direction == "D")
+        {
+            AddTrenchesInYRanche(trenches, x, y, distance, color);
+            y += distance;
         }
         else
         {
-            if(!line.empty())
-            {
-                std::vector<std::string> split;
-                StringHandling::SplitOnString(line, split, " = ");
-                network[split[0]] = GetNodeFromString(split[1]);
-            }
+            std::cout << "INVALLID DIRECTION!?\n";
         }
     }
 
-    std::string position = "AAA";
-    size_t instructionIndex = 0;
-    int numberOfSteps = 0;
-
-    while(position != "ZZZ")
-    {
-        const Node& node = network[position];
-        if(instructions[instructionIndex] == 'L')
-        {
-            position = std::get<0>(node);
-        }
-        else
-        {
-            position = std::get<1>(node);
-        }
-        instructionIndex = ++instructionIndex % instructions.length();
-        ++numberOfSteps;
-    }
-
-    std::cout << "Number of steps taken: " << numberOfSteps << "\n";
+    const int interiorSize = GetTrenchInteriorSize(trenches);
+    std::cout << "InteriorSize: " << interiorSize << "\n";
 }
-
-std::vector<std::string> GetStartPositions(const Network& network)
-{
-    std::vector<std::string> positions;
-    for(const auto& mapItem : network)
-    {
-        const std::string nodeKey = mapItem.first;
-        if(nodeKey[2] == 'A')
-        {
-            positions.push_back(nodeKey);
-        }
-    }
-    return positions;
-}
-
-bool EndReached(std::vector<std::string>& positions)
-{
-    for(const auto& position : positions)
-    {
-        if(position[2] != 'Z')
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-int GcdOfArray(std::vector<int>& arr, int idx)
-{ 
-    if (idx == arr.size() - 1) { 
-        return arr[idx]; 
-    } 
-    int a = arr[idx]; 
-    int b = GcdOfArray(arr, idx + 1); 
-    return std::gcd(a, b); // __gcd(a,b) is inbuilt library function 
-} 
 
 void SecondPart()
 {
@@ -113,81 +117,14 @@ void SecondPart()
     Utils::FileIo fileIo("C:/projects/AdventOfCode/src/input.txt");
     const std::vector<std::string> filePerLine = fileIo.GetFileContent();
 
-    std::string instructions;
-    Network network;
-
-    for(const auto& line : filePerLine)
-    {
-        if(instructions.length() == 0)
-        {
-            instructions = line;
-        }
-        else
-        {
-            if(!line.empty())
-            {
-                std::vector<std::string> split;
-                StringHandling::SplitOnString(line, split, " = ");
-                network[split[0]] = GetNodeFromString(split[1]);
-            }
-        }
-    }
-
-    std::vector<std::string> positions = GetStartPositions(network);
-    std::vector<int> numberOfStepsTaken;
-    for(auto& position : positions)
-    {
-        size_t instructionIndex = 0;
-        int numberOfSteps = 0;
-        while(position[2] != 'Z')
-        {
-            const Node& node = network[position];
-            if(instructions[instructionIndex] == 'L')
-            {
-                position = std::get<0>(node);
-            }
-            else
-            {
-                position = std::get<1>(node);
-            }
-            instructionIndex = ++instructionIndex % instructions.length();
-            ++numberOfSteps;
-        }
-        numberOfStepsTaken.push_back(numberOfSteps);
-    }
-
-    std::sort(numberOfStepsTaken.begin(), numberOfStepsTaken.end());
-    unsigned long long multiplier = GcdOfArray(numberOfStepsTaken, 0);
-
-
-    // while(!allMatch)
-    // {
-    //     unsigned long long numberOfSteps = numberOfStepsTaken[0] * multiplier;
-    //     bool noLeftOver = true;
-    //     for(const int steps : numberOfStepsTaken)
-    //     {
-    //         if(noLeftOver)
-    //         {
-    //             noLeftOver &= numberOfSteps % steps == 0;
-    //         }
-    //     }
-        
-    //     if(noLeftOver)
-    //     {
-    //         allMatch = true;
-    //     }
-    //     ++multiplier;
-    // }
-
-    std::cout << "Number of steps taken: " << numberOfStepsTaken[0] * multiplier << "\n";
 }
 
 int main()
 {
     std::cout << "Advent of code main\n";
 
-    // FirstPart();
-    SecondPart();
+    FirstPart();
+    // SecondPart();
 
     return 0;
 }
