04f47a863760c66c3f9ec01e190a26bc362941d4
Harmen Moll <harmen.moll@delem.com>
2023-12-21 07:24:17 +0100

    Intermediate day 20

--- src/main.cpp
+++ src/main.cpp
@@ -3,62 +3,215 @@
 #include <map>
 #include <stdexcept>
 #include <vector>
+#include <queue>
 
 #include "Utils/FileIO.hpp"
 #include "Utils/StringHandling.hpp"
 
-struct Trench
+struct Signal
 {
-    Trench(const int x, const int y, const std::string color)
-    : mX(x)
-    , mY(y)
-    , mColor(color)
-    {}
-
-    bool operator<(const Trench& other)
-    {
-        return mX < other.mX;
-    }
-
-    bool operator==(const Trench& other)
-    {
-        return mX == other.mX && mY == other.mY;
-    }
-
-    int mX;
-    int mY;
-    std::string mColor;
-
-    Trench* mNabour;
-    int mDistance = 0;
+    bool value;
+    std::string source;
+    std::string target;
 };
 
-typedef std::map<int, std::vector<Trench>> Trenches;
+typedef std::queue<Signal> Signals;
+Signals g_signals;
 
-int GetTrenchInteriorSize(Trenches& trenches)
+struct Module
 {
-    int accumulate = 0;
-    for(auto& rowTuple : trenches)
-    {
-        auto& row = std::get<1>(rowTuple);
-        std::sort(row.begin(), row.end());
+    Module(const std::string& name, std::vector<std::string> targets)
+    : mName(name)
+    , mTargets(targets)
+    {}
 
-        const int distanceToLeft = row[row.size()-1].mX - row[0].mX +1;
-        accumulate += distanceToLeft;
+    virtual ~Module(){}
+
+    virtual void Process(const bool signal){};
+
+    bool Output() const
+    {
+        return mState;
     }
 
-    return accumulate;
+    void PutOutputToSignals(const bool signal)
+    {
+        for(const auto& target : mTargets)
+        {
+            g_signals.push({signal, mName, target});
+        }
+    }
+
+    virtual void AddConnection(const Module* module)
+    {}
+
+    bool HasNameInTargets(const std::string& name) const
+    {
+        const auto found = std::find_if(mTargets.begin(), mTargets.end(), [&name](const std::string& targetName){ return name == targetName; });
+        return found != mTargets.end();
+    }
+
+    std::string mName;
+    bool mState = false;
+    std::vector<std::string> mTargets;
+};
+
+struct Button : public Module
+{
+    Button()
+    : Module("button", {"broadcaster"})
+    {}
+    
+    void Process(const bool signal) override
+    {
+        PutOutputToSignals(signal);
+    };
+};
+
+struct Broadcaster : public Module
+{
+    Broadcaster(const std::string& name, std::vector<std::string> targets)
+    : Module(name, targets)
+    {}
+
+    void Process(const bool signal)
+    {
+        PutOutputToSignals(signal);
+    }
+};
+
+struct FlipFlop : public Module
+{
+    FlipFlop(const std::string& name, std::vector<std::string> targets)
+    : Module(name, targets)
+    {}
+
+    void Process(const bool signal) override
+    {
+        if(!signal)
+        {
+            mState = !mState;
+            PutOutputToSignals(mState);
+        }
+    }
+};
+
+struct Conjunction : public Module
+{
+    Conjunction(const std::string& name, std::vector<std::string> targets)
+    : Module(name, targets)
+    {
+        mState = true;
+    }
+
+    void AddConnection(const Module* module) override
+    {
+        incommingConnections.push_back(module);
+    }
+
+    void Process(const bool) override
+    {
+        bool allHigh = true;
+        for(const auto* module : incommingConnections)
+        {
+            allHigh &= module->Output();
+            if(!allHigh)
+                break;
+        }
+
+        if(allHigh)
+        {
+            mState = !allHigh;
+            PutOutputToSignals(mState);
+        }
+        else
+        {
+            mState = true;
+            PutOutputToSignals(mState);
+        }
+    }
+
+    std::vector<const Module*> incommingConnections;
+};
+
+typedef std::map<std::string, Module*> Modules;
+
+void AddToModules(const std::string& input, Modules& modules)
+{
+    std::vector<std::string> split;
+    StringHandling::SplitOnString(input, split, " -> ");
+
+    std::vector<std::string> targets;
+    StringHandling::SplitOnString(split[1], targets, ", ");
+    const std::string& name = split[0];
+    if(name == "broadcaster")
+    {
+        Broadcaster* broadcaster = new Broadcaster(name, targets);
+        modules[name] = broadcaster;
+    }
+    else
+    {
+        const char& type = name[0];
+        const std::string typelessName = name.substr(1);
+        if(type == '%')
+        {
+            FlipFlop* flipFlop = new FlipFlop(typelessName, targets);
+            modules[typelessName] = flipFlop;
+        }
+        else if(type == '&')
+        {
+            Conjunction* conjunction = new Conjunction(typelessName, targets);
+            for(const auto modulePair : modules)
+            {
+                const auto* module = modulePair.second;
+               if(module->HasNameInTargets(typelessName))
+               {
+                    conjunction->AddConnection(module);
+               }
+            }
+            modules[typelessName] = conjunction;
+        }
+    }
 }
 
-void AddTrenchesInYRanche(Trenches& trenches, const int x, const int yStart, const int numberOfTrenches, const std::string color)
+std::string GetOutputString(const bool value)
 {
-    for(int index = yStart; index != yStart+numberOfTrenches+1; ++index)
+    if(value)
     {
-        const auto found = std::find_if(trenches[index].begin(), trenches[index].end(), [&x](const auto& trench){ return trench.mX == x;});
-        if(found == trenches[index].end())
+        return " -high-> ";
+    }
+
+    return " -low-> ";
+}
+
+void PushButton(Modules& modules, uint64_t& high, uint64_t& low)
+{
+    modules["button"]->Process(false);
+    while(!g_signals.empty())
+    {
+        auto& signalIterator = g_signals.front();
+        std::cout << signalIterator.source << GetOutputString(signalIterator.value) << signalIterator.target << "\n";
+
+        try
         {
-            trenches[index].push_back({x, index, color});
+            modules.at(signalIterator.target)->Process(signalIterator.value);
         }
+        catch(const std::out_of_range&)
+        {
+            // Do nothing?
+            std::cout << "Unknown target: " << signalIterator.target << "\n";
+        }
+
+        if(signalIterator.value)
+        {
+            ++high;
+        }
+        else
+        {
+            ++low;
+        }
+        
+        g_signals.pop();
     }
 }
 
@@ -68,47 +221,41 @@ void FirstPart()
     Utils::FileIo fileIo("C:/projects/AdventOfCode/src/input.txt");
     const std::vector<std::string> filePerLine = fileIo.GetFileContent();
 
-    Trenches trenches;
+    std::string boadcast;
+    Modules modules;
+    modules["button"] = new Button();
 
-    int x = 0;
-    int y = 0;
+    std::vector<std::string> addAtEnd;
     for(const auto& line : filePerLine)
     {
-        std::vector<std::string> split;
-        StringHandling::SplitStringOnCharacter(line, split, ' ');
-
-        const std::string& direction = split[0];
-        const int distance = std::stoi(split[1]);
-        const std::string color = split[2];
-
-        if(direction == "R")
+        if(line.find("&") != line.npos)
         {
-            x += distance;
-            trenches[y].push_back({x, y, color});
-        }
-        else if(direction == "L")
-        {
-            x -= distance;
-            trenches[y].push_back({x, y, color});
-        }
-        else if(direction == "U")
-        {
-            AddTrenchesInYRanche(trenches, x, y - distance, distance, color);
-            y -= distance;
-        }
-        else if(direction == "D")
-        {
-            AddTrenchesInYRanche(trenches, x, y, distance, color);
-            y += distance;
+            addAtEnd.push_back(line);
         }
         else
         {
-            std::cout << "INVALLID DIRECTION!?\n";
+            AddToModules(line, modules);
         }
     }
 
-    const int interiorSize = GetTrenchInteriorSize(trenches);
-    std::cout << "InteriorSize: " << interiorSize << "\n";
+    for(const auto& line : addAtEnd)
+    {
+        AddToModules(line, modules);
+    }
+
+    uint64_t high = 0u;
+    uint64_t low = 0u;
+    for(size_t buttonPushes = 0u; buttonPushes != 1000; ++buttonPushes)
+    {
+        PushButton(modules, high, low);
+    }
+
+    std::cout << "result: " << high * low << "\n";
+
+    for(auto& module : modules)
+    {
+        delete module.second;
+    }
 }
 
 void SecondPart()
